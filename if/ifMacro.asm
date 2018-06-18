3586d3de49d44a0c2da54bbb8c1e2ebe9c22879f
Adarley <adarleyunb@gmail.com>
2018-06-18 08:43:13 -0300

    Macro para if

---
+++ if/ifMacro.asm
@@ -0,0 +1,42 @@
+%include "asm_io.inc"
+
+%macro ifMacro 1
+	cmp %1,1
+	je %$true
+	j %false
+	%$true:
+		mov eax, msgTrue
+		call print_string
+		mov eax, newline
+		call print_string
+	%$false:
+		mov eax, msgFalse
+		call print_string
+		mov eax, newline
+		call print_string
+	%pop
+%endmacro
+
+
+section .dat
+newline 		db "", 0xa, 0
+msgTrue			db "True", 0
+msgFalse		db "False", 0
+
+segment .bss
+
+segment .text
+  global  main
+	main:
+		enter	0,0
+		pusha
+
+mov ecx, 1 ;True
+;mov ecx, 0 ;False
+
+ifMacro ecx
+
+popa
+mov	eax, 0
+leave
+ret
