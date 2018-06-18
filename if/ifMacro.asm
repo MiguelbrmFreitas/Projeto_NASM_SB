048b687b00ff1844eacc95421b3f7c4d03f8b4aa
Adarley <adarleyunb@gmail.com>
2018-06-18 09:58:25 -0300

    correcao if

--- if/ifMacro.asm
+++ if/ifMacro.asm
@@ -16,6 +16,31 @@
 		call print_string
 %endmacro
 
+%macro if 1
+	%push if
+	j%−1 %$ifnot
+%endmacro
+%macro else 0
+	%ifctx if
+		%repl else
+		jmp %$ifend
+		%$ifnot:
+	%else
+		%error "expected ‘if’ before ‘else’"
+	%endif
+%endmacro
+%macro endif 0
+	%ifctx if
+		%$ifnot:
+		%pop
+	%elifctx else
+		%$ifend:
+		%pop
+	%else
+		%error "expected ‘if’ or ‘else’ before ‘endif’"
+	%endif
+%endmacro
+
 
 section .dat
 newline 	db "", 0xa, 0
@@ -33,7 +58,19 @@ segment .text
 mov ecx, 1 ;True
 ;mov ecx, 0 ;False
 
-ifMacro ecx
+cmp ecx,1
+if e
+	mov eax, msgTrue
+	call print_string
+	mov eax, newline
+	call print_string
+else
+	mov eax, msgTrue
+	call print_string
+	mov eax, newline
+	call print_string
+endif
+
 
 popa
 mov	eax, 0
