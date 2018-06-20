1ad8250f5c0a61e205226e5cae816ed5ebd528ef
douglasshiro <douglasshiro@hotmail.com>
2018-06-20 08:46:05 -0300

    fibonacci negativo

--- fibonacci/fibonacci.asm
+++ fibonacci/fibonacci.asm
@@ -3,7 +3,10 @@
 %include "asm_io.inc"
 
 %macro fibonacci 1
-	mov ecx, %1
+	mov ecx, %1			;coloca em ecx valor do termo de fibonacci
+	cmp ecx, 0
+	jl  %%fim_err
+
 	cmp ecx, 2 			;caso termo <= 2
 
 	ja %%fibo			;caso termo >= 3
@@ -19,23 +22,29 @@
 
 		sub ecx, 2		;calcula fibo a partir do termo 3
 
-		mov ebx, 1
-		mov eax, 1
+		mov ebx, 1		;inicializa sequencia fib(1) = 1
+		mov eax, 1		;inicializa sequencia fib(2) = 1
 
 	%%proximo:
-		xchg eax, ebx
-		add eax, ebx
+		xchg eax, ebx	;passa sequencia para o proximo
+		add eax, ebx	;soma termo anterior e corrente
 
-		sub ecx, 1
-		cmp ecx, 0
-		jnz %%proximo
+		sub ecx, 1		;subtrai contador de termos
+		cmp ecx, 0		;caso termo encontrado termina
+		jnz %%proximo	;senao calcula proximo
 
-	pop ebx
+	pop ebx				;volta registrador utilizado
+	jmp %%fim
+
+	%%fim_err:
+		mov eax, -1
 	%%fim:
+
 %endmacro
 
 section .dat
 newline 		db "", 0xa, 0
+err_msg			db "numero invalido...", 0xa, 0
 
 segment .bss
 
@@ -48,10 +57,20 @@ global  main
 
 fibonacci 7
 
-call print_int
-mov eax, newline
-call print_string
+cmp eax, -1
+jne fim_fibo
+	mov eax, err_msg
+	call print_string
+	mov eax, newline
+	call print_string
+	jmp fim
 
+fim_fibo:
+	call print_int
+	mov eax, newline
+	call print_string
+
+fim:
 popa
 mov	eax, 0
 leave
